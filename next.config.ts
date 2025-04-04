717540447e23df86864dd54487d959a922d30d38
correju <julian@ruedata.com>
2025-04-04 14:25:21 -0500

    fix: added redirections to solve 4xx

--- next.config.ts
+++ next.config.ts
@@ -7,24 +7,74 @@ const nextConfig: NextConfig = {
   async redirects() {
     return [
       {
-        source: '/:locale/about/',
-        destination: '/:locale/',
-        permanent: true,
+      source: '/:locale/about/',
+      destination: '/:locale/',
+      permanent: true,
       },
       {
-        source: '/:locale/contact/',
-        destination: '/:locale/',
-        permanent: true,
+      source: '/:locale/contact/',
+      destination: '/:locale/',
+      permanent: true,
       },
       {
-        source: '/:locale/blog',
-        destination: '/:locale/',
-        permanent: true,
+      source: '/:locale/blog',
+      destination: '/:locale/',
+      permanent: true,
       },
       {
-        source: '/:locale/get-started',
-        destination: '/:locale/demo',
-        permanent: true,
+      source: '/:locale/get-started',
+      destination: '/:locale/demo',
+      permanent: true,
+      },
+      {
+      source: '/:locale/8773163504.htm',
+      destination: '/:locale/',
+      permanent: true,
+      },
+      {
+      source: '/:locale/53993220678.htm',
+      destination: '/:locale/',
+      permanent: true,
+      },
+      {
+      source: '/:locale/84568287163.htm',
+      destination: '/:locale/',
+      permanent: true,
+      },
+      {
+      source: '/:locale/53993171989.htm',
+      destination: '/:locale/',
+      permanent: true,
+      },
+      {
+      source: '/:locale/71355232439.htm',
+      destination: '/:locale/',
+      permanent: true,
+      },
+      {
+      source: '/:locale/84568231924.htm',
+      destination: '/:locale/',
+      permanent: true,
+      },
+      {
+      source: '/:locale/84568166505.htm',
+      destination: '/:locale/',
+      permanent: true,
+      },
+      {
+      source: '/:locale/79626259384.htm',
+      destination: '/:locale/',
+      permanent: true,
+      },
+      {
+      source: '/:locale/93365214670.htm',
+      destination: '/:locale/',
+      permanent: true,
+      },
+      {
+      source: '/:locale/71355155971.htm',
+      destination: '/:locale/',
+      permanent: true,
       }
     ]
   },
