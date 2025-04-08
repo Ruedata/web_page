e9305531dd9cf116b36bde6b8c6f7004637730a8
correju <julian@ruedata.com>
2025-04-07 19:52:35 -0500

    fix: reduirect all paths ending with .htm

--- next.config.ts
+++ next.config.ts
@@ -27,53 +27,8 @@ const nextConfig: NextConfig = {
         permanent: true,
       },
       {
-        source: '/:locale/8773163504.htm',
-        destination: '/:locale/',
-        permanent: true,
-      },
-      {
-        source: '/:locale/53993220678.htm',
-        destination: '/:locale/',
-        permanent: true,
-      },
-      {
-        source: '/:locale/84568287163.htm',
-        destination: '/:locale/',
-        permanent: true,
-      },
-      {
-        source: '/:locale/53993171989.htm',
-        destination: '/:locale/',
-        permanent: true,
-      },
-      {
-        source: '/:locale/71355232439.htm',
-        destination: '/:locale/',
-        permanent: true,
-      },
-      {
-        source: '/:locale/84568231924.htm',
-        destination: '/:locale/',
-        permanent: true,
-      },
-      {
-        source: '/:locale/84568166505.htm',
-        destination: '/:locale/',
-        permanent: true,
-      },
-      {
-        source: '/:locale/79626259384.htm',
-        destination: '/:locale/',
-        permanent: true,
-      },
-      {
-        source: '/:locale/93365214670.htm',
-        destination: '/:locale/',
-        permanent: true,
-      },
-      {
-        source: '/:locale/71355155971.htm',
-        destination: '/:locale/',
+        source: '/:path*.htm',
+        destination: '/',
         permanent: true,
       },
       {
